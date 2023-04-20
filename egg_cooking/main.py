1c25057df5eb85f469a931e0ade5a244843d18e3
samy <samyberrahou@gmail.com>
2023-04-20 18:43:45 +0200

    egg cooking version 1.1

--- egg_cooking/main.py
+++ egg_cooking/main.py
@@ -1,22 +1,39 @@
 import time
-
-
-def minuteur(durée: int):
-    min = durée // 60
-    sec = durée - min * 60
-    print(f"{min:02d} min")
-    print(f"{sec:02d} sec")
-
+import beepy
 
 print("Programme de temps de cuisson des oeufs")
 print("1- Oeufs à la coque")
 print("2- Oeufs mollets")
 print("3- Oeufs durs\n")
+while True:
+    choix = input("Choisissez 1 2 ou 3 ")
+    if choix == "1" or choix == "2" or choix == "3":
+        break
+    print("ERREUR: vous devez choisir 1, 2 ou 3\n")
 
-choix = input("Choisissez 1 2 ou 3 ")
+duree = 0
 if choix == "1":
-    minuteur(90)
+    duree = 90
 if choix == "2":
-    minuteur(120)
+    duree = 120
 if choix == "3":
-    minuteur(150)
+    duree = 150
+
+while True:
+    for i in range(10):
+        time.sleep(1)
+        print(".", end="", flush=True)
+        duree -= 1
+        if duree <= 0:
+            break
+
+    if duree <= 0:
+        break
+    min = duree // 60  # division entiere (pas de virgule)
+    sec = duree - min * 60
+    print()
+    print(f"Temps restant : {min:02d}:{sec:02d}", end="", flush=True)
+
+print()
+print("Cuisson terminée")
+beepy.beep(sound="ping")
