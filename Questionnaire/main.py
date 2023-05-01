422e525cd7889bccba31ecdb79a0dcbb9563724c
samy <samyberrahou@gmail.com>
2023-05-01 17:43:04 +0200

    questionnaire version 1.1
    Exercice distance chauffeur

--- Questionnaire/main.py
+++ Questionnaire/main.py
@@ -26,3 +26,36 @@ poser_question("Quelle est la capitale de l'italie ?", "Florence", "Rome",
                "Milan", "Venice", "b")
 
 print("Score final: ", score)
+
+
+# Exercice distance chauffeur ------------------------------------
+
+
+nom_chauffeur = ["mark", "patrick", "youyou", "zoubir", "moha"]
+distance_chauffeur_km = [1.5, 4, 2.6, 0.6, 2.2]
+
+
+# V2 :
+
+noms_et_distances = [("mark", 1.5), ("patrick", 4), ("youyou", 2.6), ("zoubir", 0.6), ("moha", 2.2)]
+
+nom_et_distance_min = noms_et_distances[0]
+for nom_et_distance in noms_et_distances:
+    if nom_et_distance[1] < nom_et_distance_min[1]:
+        nom_et_distance_min = nom_et_distance
+
+print("Le chauffeur le plus proche est:", nom_et_distance_min[0], ", il se trouve à: ", nom_et_distance_min[1], "km")
+
+# V1 :
+
+# index_min = 0
+# distance_min = distance_chauffeur_km[0]
+# for i in range(len(distance_chauffeur_km)):
+#     distance = distance_chauffeur_km[i]
+#     if distance < distance_min:
+#         distance_min = distance
+#         index_min = i
+#
+# print("La distance minimale est: ", distance_min)
+# print("index de la distance minimale: ", index_min)
+# print("Le chauffeur le plus proche ets:", nom_chauffeur[index_min])
