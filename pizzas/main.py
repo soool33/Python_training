7548a17ca71dbcdaf316522adf0a3813e12aba4b
samy <samyberrahou@gmail.com>
2023-05-01 18:29:51 +0200

    pizzas version 1.1
    ajouter pizza

--- pizzas/main.py
+++ pizzas/main.py
@@ -1,8 +1,22 @@
 def afficher(collection):
-    print(f"------------ Liste des pizzas ({len(collection)}) ------------")
+    nb_pizzas = len(collection)
+    if nb_pizzas == 0:
+        print("Il n y a aucune pizza a afficher")
+        return
+
+    print(f"------------ Liste des pizzas ({nb_pizzas}) ------------")
     for i in collection:
         print(i)
+    print()
+    print("La premiere pizza:", collection[0])
+    print("La derniere pizza:", collection[-1])
 
 
-pizzas = ("4 fromages", "végétarienne", "hawai", "calzon")
+def ajouter_pizza(collection):
+    p = input("Ajoutez votre pizza: ")
+    collection.append(p)
+
+
+pizzas = ["4 fromages", "végétarienne", "hawai", "calzon"]
+ajouter_pizza(pizzas)
 afficher(pizzas)
