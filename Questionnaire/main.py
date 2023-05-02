851c94a546af3247d09d16c507354b5d88c0403c
samy <samyberrahou@gmail.com>
2023-05-02 17:59:59 +0200

    questionnaire version 1.2
    amélioration avec les listes

--- Questionnaire/main.py
+++ Questionnaire/main.py
@@ -1,16 +1,19 @@
 # Questionnaire
-def poser_question(question, r1, r2, r3, r4, choix_bonne_reponse):
+def poser_question(question):
+    choix = question[1]
+    bonne_reponse = question[2]
     global score
-    print("Score: ", score)
+    # print("Score: ", score)
     print("Question: ")
-    print(" ", question)
-    print("   (a)", r1)
-    print("   (b)", r2)
-    print("   (c)", r3)
-    print("   (d)", r4)
+    print(" ", question[0])
+    for i in range(len(choix)):
+        print("   ", i+1, "-", choix[i])
+
     print()
-    reponse = input("Votre réponse: ")
-    if reponse == choix_bonne_reponse:
+    reponse_str = input(f"Votre réponse(entre 1 et {str(len(choix))}): ")
+    reponse_int = int(reponse_str)
+
+    if choix[reponse_int-1].lower() == bonne_reponse.lower():
         print("Bonne réponse")
         score += 1
     else:
@@ -19,32 +22,33 @@ def poser_question(question, r1, r2, r3, r4, choix_bonne_reponse):
     print()
 
 
-score = 10
-poser_question("Quelle est la capitale de la france ?", "Marseille", "Nice",
-               "Paris", "Bordeaux", "c")
-poser_question("Quelle est la capitale de l'italie ?", "Florence", "Rome",
-               "Milan", "Venice", "b")
+score = 0
+
+question1 = ("Quelle est la capitale de la france ?", ("Marseille", "Nice", "Paris", "Bordeaux", "Lille"), "Paris")
+question2 = ("Quelle est la capitale de l'italie ?", ("Florence", "Rome", "Milan", "Venice"), "Rome")
+
+poser_question(question1)
+poser_question(question2)
 
 print("Score final: ", score)
-
 
 # Exercice distance chauffeur ------------------------------------
 
 
-nom_chauffeur = ["mark", "patrick", "youyou", "zoubir", "moha"]
-distance_chauffeur_km = [1.5, 4, 2.6, 0.6, 2.2]
-
-
-# V2 :
-
-noms_et_distances = [("mark", 1.5), ("patrick", 4), ("youyou", 2.6), ("zoubir", 0.6), ("moha", 2.2)]
-
-nom_et_distance_min = noms_et_distances[0]
-for nom_et_distance in noms_et_distances:
-    if nom_et_distance[1] < nom_et_distance_min[1]:
-        nom_et_distance_min = nom_et_distance
-
-print("Le chauffeur le plus proche est:", nom_et_distance_min[0], ", il se trouve à: ", nom_et_distance_min[1], "km")
+# nom_chauffeur = ["mark", "patrick", "youyou", "zoubir", "moha"]
+# distance_chauffeur_km = [1.5, 4, 2.6, 0.6, 2.2]
+#
+#
+# # V2 :
+#
+# noms_et_distances = [("mark", 1.5), ("patrick", 4), ("youyou", 2.6), ("zoubir", 0.6), ("moha", 2.2)]
+#
+# nom_et_distance_min = noms_et_distances[0]
+# for nom_et_distance in noms_et_distances:
+#     if nom_et_distance[1] < nom_et_distance_min[1]:
+#         nom_et_distance_min = nom_et_distance
+#
+# print("Le chauffeur le plus proche est:", nom_et_distance_min[0], ", il se trouve à: ", nom_et_distance_min[1], "km")
 
 # V1 :
 
